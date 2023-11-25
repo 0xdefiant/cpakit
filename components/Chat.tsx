14e3d726f9dfdbf0060ff4721d916a0da9ac955f
0xdefiant <anthony@defisteward.com>
2023-11-25 11:23:57 -0800

    chat

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,27 +1,29 @@
 'use client';
- 
+
+import { useState } from 'react';
 import { useChat } from 'ai/react';
 import { Button } from '@/components/ui/button'
 import { Input } from './ui/input';
 import { useSession,  } from 'next-auth/react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { ShadowIcon, ClipboardIcon } from '@radix-ui/react-icons';
+import { ShadowIcon, ClipboardIcon, CheckIcon } from '@radix-ui/react-icons';
 
  
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   const session = useSession();
+  const [copiedMessageId, setCopiedMessageId] = useState(null);
 
-  const copyToClipboard = (content: string ) => {
+  const copyToClipboard = (content: string, messageId: string) => {
     navigator.clipboard.writeText(content).then(() => {
-      // Optionally, you can display a message confirming the text was copied
-      console.log('Text copied to clipboard');
+      setCopiedMessageId(messageId);
+      setTimeout(() => setCopiedMessageId(null), 3000);
     });
   };
  
   return (
-    <main className="mx-auto w-full h-screen max-w-lg p-24">
-      <section className="mb-auto m">
+    <div className="mx-auto w-full max-w-lg">
+      <section className="mb-auto">
         {messages.map(m => (
           <div className="flex items-start mb-4" key={m.id}>
             {m.role === 'user' ? (
@@ -39,26 +41,38 @@ export default function Chat() {
                 )}
                 <div className="flex-1">
                   <span>{m.content}</span>
-                  <ClipboardIcon className="ml-2 cursor-pointer" onClick={() => copyToClipboard(m.content)} />
                 </div>
-              </>
-            ) : (
-              <>
-                <ShadowIcon className="h-6 w-6 mr-2" />
-                <div className="flex-1">
-                  <span>{m.content}</span>
-                  <ClipboardIcon className="ml-2 cursor-pointer" onClick={() => copyToClipboard(m.content)} />
+                <div className="flex-1"> 
+                  {copiedMessageId === m.id ? (
+                    <CheckIcon className="ml-2" />
+                  ) : (
+                    <ClipboardIcon className="ml-2 cursor-pointer" onClick={() => copyToClipboard(m.content, m.id)} />
+                  )}
                 </div>
-              </>
-            )}
-          </div>
-        ))}
-      </section>
+                </>
+              ) : (
+                <>
+                  <ShadowIcon className="h-6 w-6 mr-2" />
+                  <div className="flex-1">
+                    <span>{m.content}</span>
+                  </div>  
+                  <div className="flex-1">
+                    {copiedMessageId === m.id ? (
+                      <CheckIcon className="ml-2" />
+                    ) : (
+                      <ClipboardIcon className="ml-2 cursor-pointer" onClick={() => copyToClipboard(m.content, m.id)} />
+                    )}
+                  </div>
+                </>
+              )}
+            </div>
+          ))}
+        </section>
 
 
-      <form className="flex space-x-4" onSubmit={handleSubmit}>
+        <form className="flex mb-8 space-x-4" onSubmit={handleSubmit}>
         <Input
-          className="rounded-md p-2"
+          className="rounded-md p-2 flex-1"
           value={input}
           onChange={handleInputChange}
           placeholder="Say something..."
@@ -70,6 +84,6 @@ export default function Chat() {
           Send
         </Button>
       </form>
-    </main>
+    </div>
   );
 }
