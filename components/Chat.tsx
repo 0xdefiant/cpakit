905bd2d9bd09e8dc9838e33bb375209a61d3d57a
0xdefiant <anthony@defisteward.com>
2023-12-11 11:51:06 -0800

    save

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useChat } from 'ai/react';
 import ReactMarkdown from 'react-markdown';
 import apiClient from '@/libs/api';
