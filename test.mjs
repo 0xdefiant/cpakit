23aba1dd2139b263b375e8e07eed2ca4ca4f12e6
0xdefiant <anthony@defisteward.com>
2024-01-10 14:42:40 -0800

    token

--- test.mjs
+++
@@ -1,5 +0,0 @@
-import getNonZeroTokenBalances from "./libs/tokenBalance";
-
-getNonZeroTokenBalances(address)
-  .then(tokens => console.log("Non-zero tokens:", tokens))
-  .catch(error => console.error("Error:", error));
