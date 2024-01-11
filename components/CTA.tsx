37fffb55c6d8edd491bf71a0b5bb6574b1c64538
0xdefiant <anthony@defisteward.com>
2024-01-10 23:41:24 -0800

    pics

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -6,9 +6,12 @@ const CTA = () => {
   return (
     <section className="relative flex justify-center items-center overflow-hidden min-h-screen">
       <div className="absolute inset-0 z-[-1]">
-        <AspectRatio ratio={16 / 9} className="w-full h-full">
-        //
-        </AspectRatio>
+          <Image
+            src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?q=80&w=1744&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Background"
+            className="rounded-md object-cover"
+            layout="fill"
+          />
       </div>
       <div className="relative z-10 p-8">
         <h2 className="font-bold text-2xl lg:text-4xl text-transparent gradient-text animate-gradient mb-8">
