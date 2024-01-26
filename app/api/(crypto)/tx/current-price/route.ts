0eca089223dea3c713db0494f200382902232c29
0xdefiant <anthony@defisteward.com>
2024-01-26 11:21:39 -0800

    current price cache implimentation

--- app/api/(crypto)/tx/current-price/route.ts
+++ app/api/(crypto)/tx/current-price/route.ts
@@ -1,5 +1,11 @@
 const apikey = process.env.MORALIS_API_KEY;
 
+const cache: { [key: string]: any } = {};
+// Function to check if the cache is stale (e.g., data older than 5 minutes)
+function isCacheStale(timestamp: number) {
+    return Date.now() - timestamp > 300000; // 5 minutes in milliseconds
+}
+
 export async function GET(request: Request) {
     const url = new URL(request.url);
     const address = url.searchParams.get('address');
@@ -12,6 +18,17 @@ export async function GET(request: Request) {
               'Content-Type': 'application/json'
             }
           });
+    }
+
+    // Check cache first
+    if (cache[address] && !isCacheStale(cache[address].timestamp)) {
+        console.log("Returning cached data");
+        return new Response(JSON.stringify({ priceJson: cache[address].data }), {
+            status: 200,
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
     }
 
     const priceResponse = await fetch(`https://deep-index.moralis.io/api/v2.2/erc20/${address}/price?chain=eth&include=percent_change`, {
@@ -28,6 +45,12 @@ export async function GET(request: Request) {
 
     const priceJson = await priceResponse.json();
     console.log("Price Response: ", priceJson)
+
+    // Update cache
+    cache[address] = {
+        timestamp: Date.now(),
+        data: priceJson
+    };
 
     return new Response(JSON.stringify({ priceJson }), {
         status: 200,
